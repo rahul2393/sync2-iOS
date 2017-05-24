22af9c17f59bf6dce58dc923999d20936ddd6d32
Ricky Kirkendall <rkirkendall@sixgill.com>
2017-05-24 10:40:39 -0700

    Initial Commit

---
+++ Sync2Tests/Sync2Tests.m
@@ -0,0 +1,39 @@
+//
+//  Sync2Tests.m
+//  Sync2Tests
+//
+//  Created by Ricky Kirkendall on 5/24/17.
+//  Copyright © 2017 Sixgill. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sync2Tests : XCTestCase
+
+@end
+
+@implementation Sync2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
