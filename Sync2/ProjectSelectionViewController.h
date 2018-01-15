39b6617a6e74e2d259d7b327d0bd57d3ad0ecc69
Ricky Kirkendall <rkirkendall@sixgill.com>
2018-01-15 13:06:53 -0800

    vc

---
+++ Sync2/ProjectSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectSelectionViewController.h
+//  Sync2
+//
+//  Created by Ricky Kirkendall on 1/15/18.
+//  Copyright © 2018 Sixgill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectSelectionViewController : UIViewController
+
+@end
---
+++ Sync2/ProjectSelectionViewController.m
@@ -0,0 +1,37 @@
+//
+//  ProjectSelectionViewController.m
+//  Sync2
+//
+//  Created by Ricky Kirkendall on 1/15/18.
+//  Copyright © 2018 Sixgill. All rights reserved.
+//
+
+#import "ProjectSelectionViewController.h"
+
+@interface ProjectSelectionViewController ()
+
+@end
+
+@implementation ProjectSelectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
