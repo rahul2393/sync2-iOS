c0b820ba551dae3fb11e91a29534778cda7845cc
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-07-25 17:12:40 +0530

    custom cell for options in notification's survey cell

---
+++ Sync2/OptionSurveyTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  OptionSurveyTableViewCell.h
+//  Sync2
+//
+//  Created by Sanchit Mittal on 25/07/18.
+//  Copyright © 2018 Sixgill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionSurveyTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *selectedImageView;
+@property (weak, nonatomic) IBOutlet UILabel *optionValueLabel;
+
+@end
