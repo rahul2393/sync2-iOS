b1a96c7002ef7a558de2a3fc64e4b376c0f736c0
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-12-03 14:16:01 +0530

    quick fix

--- Sync2/SettingsTableViewController.m
+++ Sync2/SettingsTableViewController.m
@@ -233,7 +233,7 @@
                 cell.keyLabel.text = @"Software Version";
                 NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
                 NSString *build = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString *)kCFBundleVersionKey];
-                cell.valueLabel.text = [NSString stringWithFormat:@"%@ , (%@)", version, build];
+                cell.valueLabel.text = [NSString stringWithFormat:@"%@ (%@)", version, build];
                 break;
             }
             default:
