e6c0440fc9ea9fc881ba4ae7540814b74b745365
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-12-19 17:12:35 +0530

    removed support for rules from sense-api

--- Sync2/SenseAPI.h
+++ Sync2/SenseAPI.h
@@ -15,7 +15,6 @@
 -(void) GetLandmarksForProject:(NSString *_Nonnull)projectId WithCompletion:(void ( ^ _Nullable )(NSArray *landmarks, NSError * _Nullable error))completed;
 -(void) GetDataChannelsWithCompletion:(void ( ^ _Nullable )(NSArray *dataChannels, NSError * _Nullable error))completed;
 -(void) GetProjectsWithCompletion:(void ( ^ _Nullable )(NSArray * projects, NSError * _Nullable error))completed;
--(void) GetRulesForProject:(NSString *_Nonnull)projectId WithCompletion:(void ( ^ _Nullable )(NSArray *rules, NSError * _Nullable error))completed;
 -(void) LoginWithEmail:(NSString *_Nonnull)email andPassword:(NSString *_Nonnull)password
         withCompletion:(void ( ^ _Nullable )(NSError * _Nullable error))completed;
 
