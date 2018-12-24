d1141e37bf66410a0204896408d48efd29aecb6e
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-12-24 14:56:13 +0530

    Camera screen added

---
+++ Sync2/CameraViewController.h
@@ -0,0 +1,17 @@
+//
+//  CameraViewController.h
+//  Sync2
+//
+//  Created by Sanchit Mittal on 24/12/18.
+//  Copyright © 2018 Sixgill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CameraViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sync2/CameraViewController.m
@@ -0,0 +1,32 @@
+//
+//  CameraViewController.m
+//  Sync2
+//
+//  Created by Sanchit Mittal on 24/12/18.
+//  Copyright © 2018 Sixgill. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
