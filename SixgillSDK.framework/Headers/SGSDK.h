a992d5654118c4a4db3d731e0102ad4c372db973
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-12-24 16:04:33 +0530

    used updated framework

---
+++ SixgillSDK.framework/Headers/SGSDK.h
@@ -0,0 +1,63 @@
+//
+//  SGSDK.h
+//  SixgillSDK
+//
+//  Created by Ricky Kirkendall on 11/30/16.
+//  Copyright © 2016 Sixgill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SensorUpdateDelegate.h"
+#import "SGLogDelegate.h"
+#import "EventQueuePolicy.h"
+#import "SGIoTDevice.h"
+#import "SGSDKConfigManager.h"
+/**
+ `SGSDK` is the wrapper class that exists for the purpose of abstracting away implemenation details and providing a clean API to the user.
+ **/
+
+@interface SGSDK : NSObject
+
+@property(nonatomic, readwrite) SGSDKConfigManager *config;
+
++(instancetype)sharedInstance;
+
+-(void)startWithAPIKey:(NSString *)apiKey;
+-(void)startWithAPIKey:(NSString *)apiKey andConfig:(SGSDKConfigManager *)config;
+-(void)startWithAPIKey:(NSString *)apiKey andConfig:(SGSDKConfigManager *)config andSuccessHandler:(nullable void (^)())successBlock andFailureHandler:(nullable void (^)(NSString *))failureBlock;
+
++(void) enable;
++(void) enableWithSuccessHandler: (void (^)())successBlock andFailureHandler:(void (^)(NSString *))failureBlock;
+
++(void) disable;
+
++(NSString *)deviceId;
+
++(void) setIngressURL:(NSString *)urlString;
+
++(void) didReceivePushNotificationPayload:(NSDictionary *)payload
+                    withCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
+
++(void) setPushToken:(NSString *)pushToken;
++(NSString *) storedPushToken;
+
++(void) forceSensorUpdate;
+
++(void) requestAlwaysPermission;
+
++(void) setMotionActivityEnabled:(BOOL)enabled;
++(BOOL) motionActivityEnabled;
+
+// Configs
+
++(void) registerForSensorUpdates:(id<SensorUpdateDelegate>)delegate;
+
+#pragma mark - Core Data
++(void) saveCoreDataContext;
+
+// Hailer Integration
+
++(void) uploadImageForHailer:(UIImage *)image andSuccessHandler:(void (^)())successBlock andFailureHandler:(void (^)(NSString *))failureBlock;
+
+@end
