bf803d3f8ed4619417efef874d7901768af968fd
Sanchit Mittal <sanchit.mittal@hotcocoasoftware.com>
2019-01-03 12:35:37 +0530

    util rule condition model class added

---
+++ Sync2/RuleCondition.h
@@ -0,0 +1,17 @@
+//
+//  RuleCondition.h
+//  Sync2
+//
+//  Created by Sanchit Mittal on 02/01/19.
+//  Copyright © 2019 Sixgill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RuleCondition : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
