e86fd00ffc8ea2656d792df8970f81c8e1e7576c
Sanchit Mittal <sanchit.mittal@hotcocoasoftware.com>
2019-01-04 19:03:26 +0530

    refactor

--- Sync2/MasterTabViewController.m
+++ Sync2/MasterTabViewController.m
@@ -105,15 +105,3 @@
 
 
 @end
-
-// size_t size = [aMsg serializedSize];
-// NSMutableData *foo = [NSMutableData dataWithCapacity:size + sizeof(size)];
-// [foo writeSize:size];
-// [foo appendData:[aMsg data]];
-//
-//
-// NSData *data = [aMsg data];
-// NSUInteger size = [aMsg length];
-// NSMutableData *foo = [NSMutableData dataWithCapacity:size + sizeof(size)];
-// [foo writeSize:size];
-// [foo appendData:data];
