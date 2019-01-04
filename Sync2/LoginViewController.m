e2cec33158fdde75805762df0cd325814a089857
Sanchit Mittal <sanchit.mittal@hotcocoasoftware.com>
2019-01-04 14:08:32 +0530

    updated framework

--- Sync2/LoginViewController.m
+++ Sync2/LoginViewController.m
@@ -38,8 +38,8 @@
     
 //    self.emailAddressField.text = @"ritik.rishu@hotcocoasoftware.com";
 //    self.passwordField.text = @"password123";
-//    self.emailAddressField.text = @"sanchit.mittal@hotcocoasoftware.com";
-//    self.passwordField.text = @"password123";
+    self.emailAddressField.text = @"sanchit.mittal@hotcocoasoftware.com";
+    self.passwordField.text = @"password123";
 //    self.emailAddressField.text = @"rahulyadavsep92@gmail.com";
 //    self.passwordField.text = @"change123*";
 //    self.emailAddressField.text = @"cvalera@sixgill.com";
